24e424ec92fae771aa696180075f79252595a2ab
PentaxJung <lich13@naver.com>
2022-12-09 22:08:47 +0900

    최적화 중

--- MyProgram/a.py
+++ MyProgram/a.py
@@ -1,55 +1,93 @@
 from pandas import read_csv, DataFrame
-from numpy import divide
+from time import process_time
 import timeit
 
 def run():
     in_file = '../2016auto.in'
     pop_file = '../pop.in'
 
+    whole_seg_time = process_time()
+
+    read_data_time = process_time()
     data = read_csv(in_file, skiprows=2, header=None, names=['O', 'D', 'Traffic'])
     data_pop = read_csv(pop_file, skiprows=1, header=None, names=['before', 'after', 'ratio'])
+    print(" 데이터 불러오기 소요시간: " + str(process_time() - read_data_time))
 
-    # O/D 매트릭스 생성(self.data_2)
+    # O/D 매트릭스 생성(self.data_OD)
+    const_OD_time = process_time()
     col = [int(i) for i in data['D'].unique()]
     row = [int(i) for i in data['O'].unique()]
-
-
     data_OD = DataFrame(columns=col, index=row)
+    print(" - OD 생성 소요시간: " + str(process_time() - const_OD_time))
+
+    finish_OD_time = process_time()
     n = 0
     for i in col:
         for j in row:
             data_OD.at[i, j] = data['Traffic'][n]
             n = n + 1
+    print(" - OD 구축 소요시간: " + str(process_time() - finish_OD_time))
+    print(" - 전체 OD 구축 소요시간: " + str(process_time() - const_OD_time))
+
+    pop_process_time = process_time()
     seg_zone_df = data_pop[data_pop.duplicated(subset='before', keep=False)].groupby('before')
-    seg_data = (seg_zone_df.get_group(name) for name, group in seg_zone_df)
+    seg_data = [seg_zone_df.get_group(name) for name, group in seg_zone_df]
     a = [dict(before=int(item.values[0][0]),
               after=[int(item['after'].iloc[i]) for i in range(len(item))],
               ratio=[item['ratio'].iloc[i] for i in range(len(item))]) for item in seg_data]
-    print(a)
+
     target_zone = [a[i]['before'] for i in range(len(a))]
     seg_zone = [a[i]['after'] for i in range(len(a))]
     pop_ratio = [a[i]['ratio'] for i in range(len(a))]
+    print(" - pop 참조 소요시간: " + str(process_time() - pop_process_time))
 
-    # Total 판별용 매트릭스 생성(data_judge_be / deep copy X)
+    start_time = process_time()
+
     data_judge_be = data_OD.copy(deep=False)
-    data_judge_be.loc['Total', :] = data_judge_be.sum(axis=0) # Total sum per column:
-    data_judge_be.loc[:, 'Total'] = data_judge_be.sum(axis=1) # Total sum per row:
+    data_judge_be.loc['Total'] = data_judge_be.sum(axis=0)  # Total sum per column:
+    data_judge_be.loc[:, 'Total'] = data_judge_be.sum(axis=1)  # Total sum per row:
 
     for i in range(len(target_zone)):
         for n in range(len(seg_zone[i])):
             data_OD.loc[seg_zone[i][n]] = data_OD.loc[target_zone[i]] * pop_ratio[i][n]
             data_OD.loc[:, seg_zone[i][n]] = data_OD.loc[:, target_zone[i]] * pop_ratio[i][n]
+    print(" - 세분화 작업 소요시간: " + str(process_time() - start_time))
 
-    for i in range(len(target_zone)):
-        data_OD.loc[target_zone[i]] = 0
-        data_OD.loc[:, target_zone[i]] = 0
+    delete_time = process_time()
+    for i in target_zone:
+        data_OD.loc[i] = 0
+        data_OD.loc[:, i] = 0
     data_OD = data_OD.astype('float64').round(2)
+    print(" - 기존 존 삭제 소요시간: " + str(process_time() - delete_time))
 
     # 세분화 후 Total 판별용 매트릭스 생성(data_judge_af)
+    judge_time = process_time()
     data_judge_af = data_OD.copy(deep=False)
     data_judge_af.loc['Total', :] = data_judge_af.sum(axis=0)
     data_judge_af.loc[:, 'Total'] = data_judge_af.sum(axis=1)
     judge = abs(data_judge_be['Total']['Total'] / data_judge_af['Total']['Total'])
     if 0.9999 < judge < 1.0001:
-        print("\nZone segmentation is successfully done!")
+        print("Zone segmentation is successfully done!")
+    else:
+        print("Error...")
+        print(" 오류 . . .")
+        print(str(judge))
+    print(" - 판별 작업 소요시간: " + str(process_time() - judge_time))
+
+    write_time = process_time()
+    file = open('../wow.txt', 'w', encoding='utf8')
+    header = 't matrices\na matrix=mf01 2016auto\n'
+    file.write(header)
+    # for i in data_OD.index:
+    #     for j in data_OD.columns:
+    #         lines = '%d\t%d\t%f\n' % (i, j, data_OD.at[i, j])
+    #         file.write(lines)
+    print(data_OD)
+    for row in data_OD.itertuples():
+        lines = str(row)
+        file.write(lines)
+    print(" - 파일 쓰기 소요시간: " + str(process_time() - write_time))
+
+    print(" - 전체 세분화 소요시간: " + str(process_time() - whole_seg_time))
+
 print(timeit.timeit(run, number=1))
--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import tkinter.ttk as ttk
 import tkinter.scrolledtext as scrolledtext
 from pandas import read_csv, DataFrame
 from numpy import divide
-from re import split
+from time import process_time
 
 
 class MainWindow(object):
@@ -137,7 +137,7 @@ class MainWindow(object):
         print ("a")
 
     def browse_button_in(self):
-        filename_in = filedialog.askopenfilename(title="Select file")
+        filename_in = filedialog.askopenfilename(title="Select file", initialfile="2016auto.in")
         if filename_in[-3:] == '.in':
             self.data = read_csv(filename_in, skiprows=2, header=None, names=['O', 'D', 'Traffic'])
             self.text_data.insert(CURRENT, "선택한 .in 파일 경로:" + filename_in +
@@ -148,7 +148,7 @@ class MainWindow(object):
             self.popup(warning_txt="올바른 .in 파일을 선택해주세요.", warning_txt_sub="현재 선택한 파일: " + filename_in)
 
     def browse_button_pop(self):
-        filename_pop = filedialog.askopenfilename(title="Select file")
+        filename_pop = filedialog.askopenfilename(title="Select file", initialfile="pop.in")
         if filename_pop[-3:] == '.in':
             self.data_pop = read_csv(filename_pop, skiprows=1, header=None, names=['before', 'after', 'ratio'])
             self.text_data.insert(END, "선택한 세분화 기준 파일 경로:" + filename_pop +
@@ -159,33 +159,6 @@ class MainWindow(object):
             self.popup(warning_txt="올바른 .in 파일을 선택해주세요.", warning_txt_sub="현재 선택한 파일: " + filename_pop)
 
     def check_expected_seg(self):
-        # self.target_zone = int(self.target_zone_sv.get())
-        # self.seg_num = int(self.seg_num_sv.get())  # get the text from entry
-        
-        # self.exp_seg_zone_num = []
-        # self.exp_seg_pop = []
-                
-        # # data_pop.in으로부터 세분화 기준 데이터베이스 생성
-        # for i in range(len(self.data_pop)):
-        #     if self.data_pop['before'][i] != self.data_pop['after'][i]:
-        #         self.exp_seg_zone_num.append(self.data_pop['after'][i])
-        #         self.exp_seg_pop.append(self.data_pop['ratio'][i])
-        
-        # 입력한 파일과 입력한 세분화 개수 일치 확인
-        # if len(self.exp_seg_pop) != self.seg_num:
-        #     self.popup(warning_txt="입력한 희망 존 세분화 개수와 입력한 세분화 기준 내용의 존 개수가 다릅니다.",
-        #                warning_txt_sub="입력한 존 세분화 개수: " + str(self.seg_num) + ", 입력한 세분화 기준 존 개수: " + str(len(self.data_pop)))
-        
-        # 세분화 기준 비율 변수 할당(self.seg_zone_pop_ratio)
-        # else:
-        #     self.seg_zone_pop_ratio = divide(self.exp_seg_pop, sum(self.exp_seg_pop))
-        #     for num in range(len(self.seg_zone_pop_ratio)):
-        #         print("Population ratio for segmenting zone %s" % str(max(self.data_2.index) + 1 + num),
-        #               ": {:.2%}".format(self.seg_zone_pop_ratio[num]))
-        #         self.text_data.insert(END, "존 %s의 인구(세분화 기준) 비율" % str(max(self.data_2.index) + 1 + num) + 
-        #                               ": {:.2%}".format(self.seg_zone_pop_ratio[num])+"\n")
-        #     self.text_data.see("end")
-        
         # 세분화할 존, 세분화 후 존 번호, 세분화 기준 추출
         seg_zone_df = self.data_pop[self.data_pop.duplicated(subset='before', keep=False)].groupby('before')
         seg_data = [seg_zone_df.get_group(name) for name, group in seg_zone_df]
@@ -216,50 +189,78 @@ class MainWindow(object):
         self.text_data.see("end")
 
     def save(self):
-        self.name = filedialog.asksaveasfilename(defaultextension=".txt")
+        self.name = filedialog.asksaveasfilename(defaultextension=".txt", initialfile="wow.txt")
         self.save_dir_msg.config(text=self.name)
 
     def run(self):
         self.text_data.insert(END, ". . . 존 세분화를 시작합니다 . . .\n\n")
         self.text_data.see("end")
 
+        start_time_const_OD = process_time()
+        self.text_data.insert(END, ". . . OD 매트릭스 생성 시작 . . .\n\n")
         # O/D 매트릭스 생성(self.data_OD)
         col = [int(i) for i in self.data['D'].unique()]
         row = [int(i) for i in self.data['O'].unique()]
+        # col = self.data['D'].unique()
+        # row = self.data['O'].unique()
         data_OD = DataFrame(columns=col, index=row)
-        
+        self.text_data.insert(END, " - 소요시간: " + str(process_time() - start_time_const_OD) + "\n\n")
+
+        start_time_finish_OD = process_time()
         n = 0
         for i in col:
             for j in row:
                 data_OD.at[i, j] = self.data['Traffic'][n]
                 n = n + 1
+        self.text_data.insert(END, " - 소요시간: " + str(process_time() - start_time_finish_OD) + "\n\n")
+        self.text_data.insert(END, ". . . OD 매트릭스 생성 완료 . . .\n\n")
+        self.text_data.insert(END, " - 전체 OD 구축 소요시간: " + str(process_time() - start_time_const_OD) + "\n\n")
+
+        self.text_data.see("end")
+
         # 세분화 시작
+        start_time_seg = process_time()
         self.segmentation(data_OD, self.target_zone, self.seg_zone, self.pop_ratio)
-        
+        self.text_data.insert(END, ". . . 존 세분화 완료 . . .\n\n")
+        self.text_data.insert(END, " - 소요시간: " + str(process_time() - start_time_seg))
+        self.text_data.see("end")
+
     def segmentation(self, data_OD, target_zone, seg_zone, pop_ratio):
         # Total 판별용 매트릭스 생성(data_judge_be / deep copy X)
         data_judge_be = data_OD.copy(deep=False)
-        data_judge_be.loc['Total', :] = data_judge_be.sum(axis=0) # Total sum per column:
+        data_judge_be.loc['Total'] = data_judge_be.sum(axis=0) # Total sum per column:
         data_judge_be.loc[:, 'Total'] = data_judge_be.sum(axis=1) # Total sum per row:
-        
+
+        start_time_seg = process_time()
+        self.text_data.insert(END, ". . . 세분화 작업 시작 . . .\n\n")
         for i in range(len(target_zone)):
             for n in range(len(seg_zone[i])):
                 data_OD.loc[seg_zone[i][n]] = data_OD.loc[target_zone[i]] * pop_ratio[i][n]
                 data_OD.loc[:, seg_zone[i][n]] = data_OD.loc[:, target_zone[i]] * pop_ratio[i][n]
-                
-        for i in range(len(target_zone)):
-            data_OD.loc[target_zone[i]] = 0
-            data_OD.loc[:, target_zone[i]] = 0
+        self.text_data.insert(END, ". . . 세분화 작업 완료 . . .\n\n")
+        self.text_data.insert(END, " - 소요시간: " + str(process_time() - start_time_seg) + "\n\n")
+
+        delete_time_seg = process_time()
+        self.text_data.insert(END, ". . . 기존 존 삭제 작업 시작 . . .\n\n")
+        for i in target_zone:
+            data_OD.loc[i] = 0
+            data_OD.loc[:, i] = 0
+        self.text_data.insert(END, ". . . 기존 존 삭제 작업 완료 . . .\n\n")
+        self.text_data.insert(END, " - 소요시간: " + str(process_time() - delete_time_seg) + "\n\n")
+
         data_OD = data_OD.astype('float64').round(2)
-        
+
+        judge_time = process_time()
+        self.text_data.insert(END, ". . . 판별 작업 시작 . . .\n\n")
         # 세분화 후 Total 판별용 매트릭스 생성(data_judge_af)
         data_judge_af = data_OD.copy(deep=False)
-        data_judge_af.loc['Total', :] = data_judge_af.sum(axis=0)
+        data_judge_af.loc['Total'] = data_judge_af.sum(axis=0)
         data_judge_af.loc[:, 'Total'] = data_judge_af.sum(axis=1)
         judge = abs(data_judge_be['Total']['Total'] / data_judge_af['Total']['Total'])
+        self.text_data.insert(END, ". . . 판별 작업 완료 . . .\n\n")
+        self.text_data.insert(END, " - 소요시간: " + str(process_time() - judge_time) + "\n\n")
+
         if 0.9999 < judge < 1.0001:
-            print("\nZone segmentation is successfully done!")
-            print(judge)
             self.text_data.insert(END, "\n존 세분화를 성공적으로 완료하였습니다!\n\n")
             self.text_data.see("end")
 
@@ -276,8 +277,7 @@ class MainWindow(object):
             for j in data_OD.columns:
                 lines = '%d\t%d\t%f\n' % (i, j, data_OD[i][j])
                 file.write(lines)
-        print("- Total of raw data:", data_judge_be['Total']['Total'],
-              "\n- Total of segmented data:", data_judge_af['Total']['Total'])
+
         self.text_data.insert(END, " - 기존 O/D 총량  :"+str(data_judge_be['Total']['Total'])+"\n")
         self.text_data.see("end")
         self.text_data.insert(END, " - 세분화 O/D 총량:"+str(data_judge_af['Total']['Total'])+"\n\n")
